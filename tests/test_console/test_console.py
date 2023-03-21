6c016680203905b046de7e62944dfa9ccbb23e26
snavy <1337snavy@gmail.com>
2023-03-21 13:04:50 +0000

    fixed unittests

---
+++ tests/test_console/test_console.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" """
+import unittest
+import console
+import subprocess
+
+
+class test_console(unittest.TestCase):
+    """ """
+
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = 'Console'
+    
+    def setUp(self):
+        """ """
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_help(self):
+        p = subprocess.Popen(
+            ['python3', 'console.py'],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+
+        out, err = p.communicate(b'help\n')
+        self.assertEqual(err, b'')
+        self.assertIsNotNone(out)
+
+        out = out.decode()
+        self.assertTrue(out.startswith('(hbnb)'))
+        self.assertIn('Documented commands (type help <topic>):', out)
+        
