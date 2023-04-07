07fd796e95b1d8006b75978defc170753d0ace45
snavy <1337snavy@gmail.com>
2023-04-07 17:24:12 +0000

    ex01

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" flask """
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def route_index():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_flashes=False)
+def route_hbnb():
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=5000)
